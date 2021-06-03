e796461cb30de2a62c815e35bc37f46fcaa6c5c5
dan <danilmelkozerov0@gmail.com>
2021-06-03 22:02:52 +0500

    dbfixes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
--- config.py
+++ config.py
@@ -16,3 +16,5 @@ def config(filename='database.ini', section='postgresql'):
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
     return db
+
+print(config())

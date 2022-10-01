ba3ccef0d0b110c3beba1035cd2dbfda40d70d56
dan <danilmelkozerov0@gmail.com>
2022-10-01 20:41:14 +0400

    quickfix

--- web/auth.py
+++ web/auth.py
@@ -5,7 +5,6 @@ import re
 from web.models import User
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from flask.helpers import flash
-from werkzeug.datastructures import cache_property
 from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
